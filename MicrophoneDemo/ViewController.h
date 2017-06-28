58616c55b07b63a707b697934fe4c2b71eb7e31e
王魏 <1137461818@qq.com>
2017-06-28 10:18:58 +0800

    first commit
    first commit

---
+++ MicrophoneDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MicrophoneDemo
+//
+//  Created by 王魏 on 2017/6/28.
+//  Copyright © 2017年 wangwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MicrophoneDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MicrophoneDemo
+//
+//  Created by 王魏 on 2017/6/28.
+//  Copyright © 2017年 wangwei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
